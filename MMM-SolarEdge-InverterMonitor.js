f816772bf91f96dde34cce2dbdb1d4e276c439d8
ecardona <enriquejesus.cardonacebrian@ts.fujitsu.com>
2019-10-20 12:36:13 +0200

    fix animation config

--- MMM-SolarEdge-InverterMonitor.js
+++ MMM-SolarEdge-InverterMonitor.js
@@ -255,16 +255,16 @@ Module.register("MMM-SolarEdge-InverterMonitor", {
 		newWrapper.id = "bar-div-" + element;
 		newWrapper.className = "progress-bar stripes";
 		let spanWrapper = document.createElement("span");
-		if(self.config.animate) {
-			spanWrapper.className = "animated";
-		}
 		spanWrapper.style.width = percent + "%";
 		if (data < 0) {
 			spanWrapper.style.marginRight = center + "%";
 		} else {
 			spanWrapper.style.marginLeft = (100 - center) + "%";
 		}
-		spanWrapper.className = (data < 0 ? "inverse" : "");
+		
+		spanWrapper.classList = [
+			self.config.animate ? "animated" : "",
+			(data < 0 ? "inverse" : "")];
 
 		spanWrapper.style.backgroundColor = "rgb(" + colorRed + ", " + colorGreen + ", " + colorBlue + ")";
 
